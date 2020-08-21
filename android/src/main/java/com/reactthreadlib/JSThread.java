c43957f9d65d1fe3b15f413dc991a5b65ae00865
Jack Pettrakool <jack.pettrakool@tildawatch.com>
2020-08-21 03:37:39 -0700

    Fixed typo name.

--- android/src/main/java/com/reactthreadlib/JSThread.java
+++ android/src/main/java/com/reactthreadlib/JSThread.java
@@ -1,4 +1,4 @@
-package com.reactthreadtib;
+package com.reactthreadlib;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
